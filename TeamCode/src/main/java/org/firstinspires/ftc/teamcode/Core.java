191873776e9c5652b94a4d159a66cb750d69ffbb
Eduardo Bomfim <71347015+ShinigamiHiruzen@users.noreply.github.com>
2020-09-17 19:09:15 -0300

    Update Core.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
@@ -5,11 +5,12 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Core {
     protected DcMotor leftMotor; // This variable will be responsible for the left engine.
-    // Place the variable responsible for the right engine here.
+    protected DcMotor rightMotor; // This variable will be responsible for the right engine.
     protected HardwareMap hwMap; // This variable will be responsible for the hardware map.
 
     public void setHardwareMap() {
         leftMotor = (DcMotor)hwMap.get("leftMotor");
+        rightMotor = (DcMotor)hwMap.get("rightMotor");
         /* Place the hardware map of the right engine here, to create do the following:
         take the engine variable you created and assign its type followed by Hardware Map.get ("here you name the variable").
         This sequence will make the robot's engine connect to the variable you created.*/
