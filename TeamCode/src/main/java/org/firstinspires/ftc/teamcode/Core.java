9a6e005b44f460b06907959555da3c1dd18d2fa5
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2020-09-18 15:02:49 -0300

    Added telemetry to hardware map, void.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
@@ -4,16 +4,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
 public class Core {
     protected DcMotor leftMotor; // This variable will be responsible for the left engine.
     protected DcMotor rightMotor; // This variable will be responsible for the right engine.
     protected HardwareMap hwMap; // This variable will be responsible for the hardware map.
     protected NormalizedColorSensor colorSensor;
+    private Telemetry tlmtr;
 
     public void setHardwareMap() {
         leftMotor = (DcMotor)hwMap.get("leftMotor");
         rightMotor = (DcMotor)hwMap.get("rightMotor");
         colorSensor = (NormalizedColorSensor)hwMap.get("colorSensor");
+        tlmtr.addData("Hardware Map -> ", "Loaded!");
 
     }
 
