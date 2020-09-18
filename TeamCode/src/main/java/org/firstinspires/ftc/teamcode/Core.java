e0d0502c2819336faace8b6a0e490023772b6418
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2020-09-18 13:49:05 -0300

    Added color sensor.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Core.java
@@ -2,16 +2,19 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.NormalizedColorSensor;
 
 public class Core {
     protected DcMotor leftMotor; // This variable will be responsible for the left engine.
     protected DcMotor rightMotor; // This variable will be responsible for the right engine.
     protected HardwareMap hwMap; // This variable will be responsible for the hardware map.
+    protected NormalizedColorSensor colorSensor;
 
     public void setHardwareMap() {
         leftMotor = (DcMotor)hwMap.get("leftMotor");
         rightMotor = (DcMotor)hwMap.get("rightMotor");
-       
+        colorSensor = (NormalizedColorSensor)hwMap.get("colorSensor");
+
     }
 
 }
