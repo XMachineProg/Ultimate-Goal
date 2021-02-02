a905ceab3caa98baa481989b4c477fe21ee71823
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-02-02 18:02:07 -0300

    Added Try and Catch to isEnginesBusy class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/EngineState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/EngineState.java
@@ -4,12 +4,17 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class EngineState {
     public String isEnginesBusy(Boolean leftEngine, Boolean rightEngine) {
-        if (leftEngine == true && rightEngine == false) {
-            return "202 " + "Only left engine is busy";
-        } else if (leftEngine == false && rightEngine == true) {
-            return "202 " + "Only right engine is busy";
-        } else {
-            return "202 " + "Both engine is busy";
+        try {
+            if (leftEngine == true && rightEngine == false) {
+                return "202 " + "Only left engine is busy";
+            } else if (leftEngine == false && rightEngine == true) {
+                return "202 " + "Only right engine is busy";
+            } else {
+                return "202 " + "Both engine is busy";
+            }
+        } catch (Exception e) {
+            return "500, Some error has occurred:   " + e.getMessage();
         }
+
     }
 }
