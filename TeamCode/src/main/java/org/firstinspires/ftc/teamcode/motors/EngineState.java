8bb031c63ecb4fe2fcae8bf23bb1facb8e133bd9
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-02-02 17:58:01 -0300

    Verify if engine is busy or not

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/EngineState.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/motors/EngineState.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.motors;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 public class EngineState {
+    public String isEnginesBusy(Boolean leftEngine, Boolean rightEngine) {
+        if (leftEngine == true && rightEngine == false) {
+            return "202 " + "Only left engine is busy";
+        } else if (leftEngine == false && rightEngine == true) {
+            return "202 " + "Only right engine is busy";
+        } else {
+            return "202 " + "Both engine is busy";
+        }
+    }
 }
