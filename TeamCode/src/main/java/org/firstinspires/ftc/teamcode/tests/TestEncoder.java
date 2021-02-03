d64cf76232e5ee6d2153e9377d678c123a9afccb
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-02-03 08:11:04 -0300

    Testing encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/TestEncoder.java
@@ -17,12 +17,14 @@ public class TestEncoder extends LinearOpMode {
 
     private DcMotor leftEngine = null;
     private DcMotor rightEngine = null;
+    private DcMotor coreMotor = null;
     private NormalizedColorSensor colorSensor = null;
     private EngineMoviment em = new EngineMoviment();
 
     public void setHardwareMap() {
         leftEngine = hardwareMap.get(DcMotor.class, "leftMotor");
         rightEngine = hardwareMap.get(DcMotor.class, "rightMotor");
+        coreMotor = hardwareMap.get(DcMotor.class, "coreMotor");
         colorSensor = (NormalizedColorSensor)hardwareMap.get("colorSensor");
         telemetry.addData("201", "Added to hardware list:" +
                 hardwareMap.getNamesOf(leftEngine) + "  " + hardwareMap.getNamesOf(rightEngine));
@@ -33,36 +35,29 @@ public class TestEncoder extends LinearOpMode {
     @Override
     public void runOpMode() {
 
+        setHardwareMap(); // Return to setHardwareMap methodleftEngine.setDirection(DcMotor.Direction.REVERSE);waitForStart();
 
-        setHardwareMap(); // Return to setHardwareMap method
-        leftEngine.setDirection(DcMotor.Direction.REVERSE);
         waitForStart();
 
-        leftEngine.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        rightEngine.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        leftEngine.getCurrentPosition();
-
-
-        // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
 
-            telemetry.addData("200", "Inialized Code");
-            telemetry.update();
-            telemetry.addData(core.getReading(colorSensor), "202");
-            telemetry.addData(core.sensorName(colorSensor), "202");
+            telemetry.addData("200", "Inialized Code");telemetry.update();
 
             telemetry.addData(String.valueOf(leftEngine.getCurrentPosition()), " | current position");
 
-            leftEngine.setTargetPosition(500);
-            leftEngine.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            leftEngine.setPower(1);
-            while (leftEngine.isBusy()) {
+            coreMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            coreMotor.setTargetPosition(90);
 
+            coreMotor.setPower(.5);
+            coreMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            while (coreMotor.isBusy()) {
+                telemetry.addData("Path", "driving");
+                telemetry.update();
             }
-            leftEngine.setPower(0);
-            leftEngine.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+            coreMotor.setPower(0);
+            telemetry.addData("Path", "complete");
+            telemetry.update();
 
             break;
         }
