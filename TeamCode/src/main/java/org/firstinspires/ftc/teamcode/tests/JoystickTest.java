2a43f90acb31ba2ab6a55e9fbe79a959be020c5a
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-02-04 10:13:39 -0300

    Acceleration method created

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/JoystickTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/JoystickTest.java
@@ -18,7 +18,14 @@ public class JoystickTest extends LinearOpMode {
     private EngineMoviment em = new EngineMoviment();
 
 
-    public void setHardwareMap() {
+    private void acelleration(double leftEnginePower, double rightEnginePower) {
+        leftEngine.setPower(leftEnginePower * 2);
+        rightEngine.setPower(rightEnginePower * 2);
+        telemetry.addData("Acelleration x 2", "left engine now: " + leftEngine.getPower() + "  right engine now: " + rightEngine.getPower());
+        telemetry.update();
+    }
+
+    private void setHardwareMap() {
         leftEngine = hardwareMap.get(DcMotor.class, "leftMotor");
         rightEngine = hardwareMap.get(DcMotor.class, "rightMotor");
         colorSensor = (NormalizedColorSensor)hardwareMap.get("colorSensor");
@@ -40,28 +47,37 @@ public class JoystickTest extends LinearOpMode {
 
         while (opModeIsActive()) {
             while (gamepad1.left_stick_y != 0) {
-                leftEnginePower = gamepad1.left_stick_y;
-                rightEnginePower = gamepad1.left_stick_y;
-                leftEngine.setPower(leftEnginePower / 2);
-                rightEngine.setPower(rightEnginePower / 2);
+                leftEnginePower = (gamepad1.left_stick_y) / 2;
+                rightEnginePower = (gamepad1.left_stick_y) / 2;
+                leftEngine.setPower(leftEnginePower);
+                rightEngine.setPower(rightEnginePower);
+                while (gamepad1.right_trigger != 0) {
+                    acelleration(leftEnginePower, rightEnginePower);
+                }
 
                 while (gamepad1.left_stick_x < 0) {
-                    leftEnginePower = gamepad1.left_stick_y * gamepad1.left_stick_x;
-                    rightEnginePower = gamepad1.left_stick_y;
-                    leftEngine.setPower(leftEnginePower / 2);
-                    rightEngine.setPower(rightEnginePower / 2);
+                    leftEnginePower = (gamepad1.left_stick_y * gamepad1.left_stick_x) / 2;
+                    rightEnginePower = (gamepad1.left_stick_y) / 2;
+                    leftEngine.setPower(leftEnginePower);
+                    rightEngine.setPower(rightEnginePower);
+                    while (gamepad1.right_trigger != 0) {
+                        acelleration(leftEnginePower, rightEnginePower);
+                    }
                 }
                 while (gamepad1.left_stick_x > 0) {
-                    leftEnginePower = gamepad1.left_stick_y;
-                    rightEnginePower = gamepad1.left_stick_y * gamepad1.left_stick_x;
-                    leftEngine.setPower(leftEnginePower / 2);
-                    rightEngine.setPower(rightEnginePower / 2);
+                    leftEnginePower = (gamepad1.left_stick_y) / 2;
+                    rightEnginePower = ( gamepad1.left_stick_y * gamepad1.left_stick_x ) / 2;
+                    leftEngine.setPower(leftEnginePower);
+                    rightEngine.setPower(rightEnginePower);
+                    while (gamepad1.right_trigger != 0) {
+                        acelleration(leftEnginePower, rightEnginePower);
+                    }
                 }
 
             }
             while (gamepad1.left_stick_y == 0) {
-                leftEnginePower = gamepad1.left_stick_y;
-                rightEnginePower = gamepad1.left_stick_y;
+                leftEnginePower = (gamepad1.left_stick_y);
+                rightEnginePower = (gamepad1.left_stick_y);
                 leftEngine.setPower(leftEnginePower);
                 rightEngine.setPower(rightEnginePower);
             }
