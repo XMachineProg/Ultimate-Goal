373c4897b775a7743d584bb8bdc86734f7490d8f
T0ny <62897985+4ntonioSouza@users.noreply.github.com>
2021-02-25 14:15:18 -0300

    Update joystick test 2
    Removed State Machine.
    Test telemetry with ftc dashboard and graph.
    test new mathematic in joystick

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/SecondJoystickTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/SecondJoystickTest.java
@@ -4,95 +4,56 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
-import static org.firstinspires.ftc.teamcode.tests.SecondJoystickTest.RobotState.ERROR;
-import static org.firstinspires.ftc.teamcode.tests.SecondJoystickTest.RobotState.IDLE;
-import static org.firstinspires.ftc.teamcode.tests.SecondJoystickTest.RobotState.MOVING;
 
 @TeleOp(name = "Iterativ Joystick Test", group = "Iterative OpMode")
 public class SecondJoystickTest extends OpMode {
 
-    enum RobotState // Robot states enum
-    {
-        IDLE,
-        MOVING,
-        ERROR
-    }
 
     private FtcDashboard dashboard = FtcDashboard.getInstance(); // Get instnace from FtcDashboard
     private Telemetry tlmtr = dashboard.getTelemetry(); //Telemtry variable
 
     private DcMotor leftMotor;
     private DcMotor rightMotor;
-
-    //private double gmX = gamepad1.left_stick_x;
-    //private double gmY = gamepad1.left_stick_y;
-
-    private RobotState robotState = IDLE;
+    private double leftPower;
+    private double rightPower;
+    private double max;
+    private double turn;
+    private double drive;
 
     @Override
     public void init() {
+
         setHardwareMap(leftMotor, rightMotor); // Add motors to hwmap list
+        leftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
-        switch (robotState) {
-            case IDLE:
-                idle();
-                break;
-            case MOVING:
-                moving();
-                break;
-            case ERROR:
-                error();
-                break;
+        drive = -gamepad1.left_stick_y;
+        turn = gamepad1.left_stick_x;
+        leftPower = drive + turn;
+        rightPower = drive - turn;
+
+        max = Math.max(Math.abs(leftPower), Math.abs(rightPower));
+        if (max > 1) {
+            leftPower /= max;
+            rightPower /= max;
         }
-        while (preProcessGamepad() == 3) {
-            // Wait changes
-            robotState = IDLE;
+        if (gamepad1.right_trigger == 0.0) {
+            leftPower /= 2.0;
+            rightPower /= 2.0;
         }
-        robotState = MOVING;
+        tlmtr.addData("Left power", leftPower);
+        tlmtr.addData("Right power", rightPower);
+        tlmtr.update();
+        leftMotor.setPower(leftPower);
+        rightMotor.setPower(rightPower);
     }
 
-    private int preProcessGamepad() {
-        if (gamepad1.left_stick_x != 0 && gamepad1.left_stick_y == 0) {
-            return 0;
-        } else if (gamepad1.left_stick_x == 0 && gamepad1.left_stick_y != 0) {
-            return 1;
-        }
-        return 3;
-    }
-
-    private void idle() {
-        leftMotor.setPower(0);
-        rightMotor.setPower(0);
-        return;
-    }
-
-    private void moving() {
-        int returnPreProcess = preProcessGamepad();
-        while (returnPreProcess == 0) {
-            leftMotor.setPower(gamepad1.left_stick_x);
-            rightMotor.setPower(gamepad1.left_stick_x);
-        }
-        while (returnPreProcess == 1) {
-            leftMotor.setPower(-gamepad1.left_stick_y);
-            rightMotor.setPower(gamepad1.left_stick_y);
-        }
-        while (returnPreProcess == 3) {
-            robotState = ERROR;
-            return;
-        }
-        robotState = IDLE;
-        return;
-    }
-
-    private void error() {
-
-    }
 
     private void setHardwareMap(DcMotor lm, DcMotor rm) {
         leftMotor = hardwareMap.get(DcMotor.class, "leftMotor");
